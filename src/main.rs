4893976ea055affc11d6581b5a3001f1ec5c88d6
theMackabu <theMackabu@gmail.com>
2023-11-13 23:25:23 -0800

    cleap-up for cc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{thread, time::Duration};
 
 #[cxx::bridge]
-mod cmd {
+pub mod process {
     unsafe extern "C++" {
-        include!("pmc/src/include/cmd.h");
-        fn run_command(name: &str, log_path: &str, command: &str) -> u64;
-        fn kill_pid(pid: u64) -> u64;
+        include!("pmc/src/include/process.h");
+        include!("pmc/src/include/bridge.h");
+
+        pub fn stop(pid: u64) -> u64;
+        pub fn run(name: &str, log_path: &str, command: &str) -> u64;
     }
 }
 
@@ -13,9 +15,9 @@ fn main() {
     let name = "example";
     let log_path = "tests/logs";
     let command = "node tests/index.js";
-    let pid = cmd::run_command(&name, &log_path, &command);
+    let pid = process::run(&name, &log_path, &command);
 
     println!("pid: {pid}");
     thread::sleep(Duration::from_millis(1000));
-    cmd::kill_pid(pid);
+    process::stop(pid);
 }
