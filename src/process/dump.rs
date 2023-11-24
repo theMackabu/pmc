951524037465e5edacd0ca12ed58ce30d2e8a5ab
theMackabu <theMackabu@gmail.com>
2023-11-23 19:33:34 -0800

    full rewrite for module system

--- src/process/dump.rs
+++ src/process/dump.rs
@@ -13,23 +13,23 @@ pub fn read() -> Runner {
     if !Exists::file(global!("pmc.dump")).unwrap() {
         let runner = Runner {
             id: Id::new(0),
-            process_list: BTreeMap::new(),
+            list: BTreeMap::new(),
         };
 
         write(&runner);
         log::info!("created dump file");
     }
 
-    file::read(global!("pmc.dump"))
+    file::read_rmp(global!("pmc.dump"))
 }
 
 pub fn write(dump: &Runner) {
-    let contents = match toml::to_string(dump) {
+    let encoded: Vec<u8> = match rmp_serde::to_vec(&dump) {
         Ok(contents) => contents,
-        Err(err) => crashln!("{} Cannot parse dump.\n{}", *helpers::FAIL, string!(err).white()),
+        Err(err) => crashln!("{} Cannot encode dump.\n{}", *helpers::FAIL, string!(err).white()),
     };
 
-    if let Err(err) = fs::write(global!("pmc.dump"), contents) {
+    if let Err(err) = fs::write(global!("pmc.dump"), encoded) {
         crashln!("{} Error writing dumpfile.\n{}", *helpers::FAIL, string!(err).white())
     }
 }
--- src/process/hash.rs
+++ src/process/hash.rs
@@ -3,10 +3,12 @@ use merkle_hash::{bytes_to_hex, Algorithm, MerkleTree};
 use std::path::PathBuf;
 
 pub fn create(path: PathBuf) -> String {
+    log::debug!("creating hash for {:?}", path);
     let tree = match MerkleTree::builder(&path.to_str().unwrap()).algorithm(Algorithm::Blake3).hash_names(false).build() {
         Ok(v) => v,
         Err(e) => crashln!("Invalid UTF-8 sequence: {}", e),
     };
 
+    log::trace!("hash {:?}", tree.root.item.hash);
     bytes_to_hex(tree.root.item.hash)
 }
