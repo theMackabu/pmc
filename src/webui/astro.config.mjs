413131114a6dbda73d7a10b30841f9e13b912436
theMackabu <theMackabu@gmail.com>
2023-12-01 01:32:12 -0800

    astro: add webui ported

--- src/webui/astro.config.mjs
+++ src/webui/astro.config.mjs
@@ -1,6 +1,8 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind(), react()]
 });
