efcb17ca981309e65c34dfcfe3d3aadbfa09f2b9
theMackabu <theMackabu@gmail.com>
2024-07-09 00:01:38 -0700

    implement charts

--- src/webui/src/components/react/status.tsx
+++ src/webui/src/components/react/status.tsx
@@ -23,13 +23,14 @@ const Status = (props: { name: string; base: string }) => {
 	const memoryUsage = useArray([], bufferLength);
 	const cpuPercentage = useArray([], bufferLength);
 
+	const [item, setItem] = useState<any>();
 	const [loaded, setLoaded] = useState(false);
 	const [live, setLive] = useState<SSE | null>(null);
 
 	const options = {
 		responsive: true,
 		maintainAspectRatio: false,
-		animation: { duration: 0.5 },
+		animation: { duration: 0 },
 		layout: {
 			padding: {
 				left: 0,
@@ -121,6 +122,8 @@ const Status = (props: { name: string; base: string }) => {
 		source.onmessage = (event) => {
 			const data = JSON.parse(event.data);
 
+			setItem(data);
+
 			memoryUsage.pushMax(data.raw.memory_usage.rss);
 			cpuPercentage.pushMax(data.raw.cpu_percent + 1);
 
@@ -149,46 +152,56 @@ const Status = (props: { name: string; base: string }) => {
 		};
 	}, []);
 
-	const stats = [
-		{ name: 'Status', stat: 'Online' },
-		{ name: 'Proccess', stat: '3' },
-		{ name: 'Errors', stat: '10' },
-		{ name: 'Crashes', stat: '2' }
-	];
-
 	if (!loaded) {
 		return <Loader />;
 	} else {
+		const stats = [
+			{ name: 'Uptime', stat: startDuration(item.daemon.uptime, false) },
+			{ name: 'Count', stat: item.daemon.process_count },
+			{ name: 'Version', stat: item.version.pkg },
+			{ name: 'Process Id', stat: item.daemon.pid },
+			{ name: 'Build date', stat: item.version.build_date },
+			{ name: 'Hash', stat: item.version.hash.slice(0, 18) },
+			{ name: 'Platform', stat: `${item.os.name} ${item.os.version} (${item.os.arch})` },
+			{ name: 'Daemon', stat: item.daemon.daemon_type }
+		];
+
 		return (
 			<Fragment>
-				<h3 className="ml-8 mt-6 mb-5 text-2xl font-bold leading-6 text-zinc-200">Overview</h3>
-				<dl className="grid grid-cols-1 gap-5 sm:grid-cols-4 px-5">
-					{stats.map((item: any) => (
-						<div key={item.name} className="overflow-hidden rounded-lg bg-zinc-900/25 border border-zinc-800 px-4 py-5 shadow sm:p-6">
-							<dt className="truncate text-sm font-medium text-zinc-400">{item.name}</dt>
-							<dd className="mt-1 text-3xl font-semibold tracking-tight text-zinc-100">{item.stat}</dd>
-						</div>
-					))}
-				</dl>
-				<h3 className="ml-8 mt-8 mb-5 text-2xl font-bold leading-6 text-zinc-200">Metrics</h3>
-				<dl className="grid grid-cols-1 gap-5 sm:grid-cols-2 px-5">
-					<div className="overflow-hidden rounded-lg bg-zinc-900/25 border border-zinc-800 shadow">
+				<div className="absolute top-2 right-3 z-[200]">
+					<span className="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-zinc-800">
+						<svg viewBox="0 0 6 6" aria-hidden="true" className="h-1.5 w-1.5 fill-green-400">
+							<circle r={3} cx={3} cy={3} />
+						</svg>
+						{props.name != 'local' ? props.name : 'Internal'}
+					</span>
+				</div>
+				<dl className="mt-8 grid grid-cols-1 gap-5 sm:grid-cols-2 px-5">
+					<div className="overflow-hidden rounded-lg bg-zinc-900/20 border border-zinc-800 shadow">
 						<dt className="truncate text-sm font-bold text-zinc-400 pt-4 px-4">CPU Usage</dt>
 						<dt className="truncate text-xl font-bold text-zinc-100 p-1 px-4">
 							{cpuPercentage.value.slice(-1)[0].toFixed(2)}
 							<span className="text-base text-zinc-400">%</span>
 						</dt>
-						<dd className="mt-2 text-3xl font-semibold tracking-tight text-zinc-100 h-52" style={chartContainerStyle}>
+						<dd className="mt-2 text-3xl font-semibold tracking-tight text-zinc-100 h-96" style={chartContainerStyle}>
 							<Line data={cpuChart} options={options} />
 						</dd>
 					</div>
-					<div className="overflow-hidden rounded-lg bg-zinc-900/25 border border-zinc-800 shadow">
+					<div className="overflow-hidden rounded-lg bg-zinc-900/20 border border-zinc-800 shadow">
 						<dt className="truncate text-sm font-bold text-zinc-400 pt-4 px-4">Memory Usage</dt>
 						<dt className="truncate text-xl font-bold text-zinc-100 p-1 px-4">{bytesToSize(memoryUsage.value.slice(-1)[0], 2)}</dt>
-						<dd className="mt-2 text-3xl font-semibold tracking-tight text-zinc-100 h-52" style={chartContainerStyle}>
+						<dd className="mt-2 text-3xl font-semibold tracking-tight text-zinc-100 h-96" style={chartContainerStyle}>
 							<Line data={memoryChart} options={options} />
 						</dd>
 					</div>
+				</dl>
+				<dl className="mt-5 pb-5 grid grid-cols-2 gap-5 lg:grid-cols-4 px-5 h-3/10">
+					{stats.map((item: any) => (
+						<div key={item.name} className="overflow-hidden rounded-lg bg-zinc-900/20 border border-zinc-800 px-4 py-5 shadow sm:p-6">
+							<dt className="truncate text-sm font-medium text-zinc-400">{item.name}</dt>
+							<dd className="mt-1 text-2xl font-semibold tracking-tight text-zinc-100">{item.stat}</dd>
+						</div>
+					))}
 				</dl>
 			</Fragment>
 		);
